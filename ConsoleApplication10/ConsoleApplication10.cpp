ed3480531af4537ddbfcd7c6b576e6256ab4ca6d
furokl <70150974+furokl@users.noreply.github.com>
2020-10-11 22:13:15 +0300

    Наследование класса RGB

--- ConsoleApplication10/ConsoleApplication10.cpp
+++ ConsoleApplication10/ConsoleApplication10.cpp
@@ -19,29 +19,46 @@
 
 using std::cout;
 using std::endl;
+using std::ostream;
 
-class RGBA
+class RGB
 {
 private:
 	uint8_t m_red;
 	uint8_t m_green;
 	uint8_t m_blue;
-	uint8_t m_alpha;
 
 public:
-	RGBA(uint8_t red = 0, uint8_t green = 0, uint8_t blue = 0, uint8_t alpha = 255)
-		: m_red(red), m_green(green), m_blue(blue), m_alpha(alpha)
+	RGB(uint8_t red = 0, uint8_t green = 0, uint8_t blue = 0)
+		: m_red(red), m_green(green), m_blue(blue)
 	{
+		assert(red >= 0 && red <= 255);
+		assert(green >= 0 && green <= 255);
+		assert(blue >= 0 && blue <= 255);
 	}
+
+	int get_red() { return m_red; }
+	int get_green() { return m_green; }
+	int get_blue() { return m_blue; }
+
+	operator int() { return (m_red+m_green+m_blue)/81; }
+	friend ostream &operator<< (ostream &out, const RGB &rgb);
+
 	void printRGBA() {
 		cout << "R = " << static_cast<int>(m_red) << ';' <<
 			" G = " << static_cast<int>(m_green) << ';' <<
-			" B = " << static_cast<int>(m_blue) << ';' <<
-			" A = " << static_cast<int>(m_alpha) << '\n';
+			" B = " << static_cast<int>(m_blue) << ';';
 	}
 };
 
-class Image : public RGBA
+ostream  &operator<< (ostream &out, const RGB &rgb) {
+	out << '(' << static_cast<int>(rgb.m_red)
+		<< ", " << static_cast<int>(rgb.m_green)
+		<< ", " << static_cast<int>(rgb.m_blue) << ')';
+	return out;
+}
+
+class Image : public RGB
 {
 private:
 	int mx;
@@ -50,7 +67,7 @@ private:
 
 public:
 	Image(int tmx = 200, int tmy = 200)
-		: mx(tmx), my(tmy)
+			: mx(tmx), my(tmy)
 	{
 		assert(tmx >= 1 && tmx <= 200);
 		assert(tmy >= 1 && tmy <= 200);
@@ -60,22 +77,22 @@ public:
 
 		cout << "\n\t*Constructor worked*\n\n";
 	}
+
 	friend bool operator== (const Image &im1, const Image &im2);
 	friend bool operator!= (const Image &im1, const Image &im2);
 	Image(const Image &im)
-		: mx(im.mx), my(im.my)
-	{
+		: mx(im.mx), my(im.my){
 		for (int i = 0; i < im.mx*im.my; i++)
 			data[i] = im.data[i];
 
 		cout << "\n\t*Copy constructor worked*\n\n";
 	}
 
-	int getMx() { return mx; }
-	int getMy() { return my; }
+	int get_x() { return mx; }
+	int get_y() { return my; }
 	int get(int x, int y) { return data[(x - 1) + (y - 1) * x]; }
 
-	void set(int x, int y, int color) {
+	void set(int x, int y, RGB color) {
 		cout << "\t\t*Setter used*\n\tsize: (" 
 			<< x << 'x' << y << ')' 
 			<< "\n\tcolor: " << color << endl;
@@ -94,7 +111,7 @@ public:
 				break;
 		}
 	}
-	void set(int x, int color) {
+	void set(int x, RGB color) {
 		cout << "\t\t*Setter used*\n\tx: " << x
 			<< "\n\tcolor: " << color << endl;
 		assert(x > 1 && x < 200);
@@ -147,9 +164,9 @@ bool operator!= (const Image &im1, const Image &im2) {
 int main() {
 	cout << "\n\n\t\tProgramm started...\n\n";
 	Image a(5, 5);
-	a.set(4, 4, 1);
-	a.set(3, 3, 2);
-	a.set(2, 2, 3);
+	a.set(4, 4, (1, 3, 4));
+	a.set(3, 3, (10, 3, 5));
+	a.set(2, 2, (100, 35, 53));
 	a.show();
 	Image b = a;
 
@@ -164,7 +181,7 @@ int main() {
 	else
 		cout << "== FALSE" << endl;
 	
-	b.set(5, 6);
+	b.set(5, (33, 33, 33));
 	a.show();
 	b.show();
 	if (a != b)
